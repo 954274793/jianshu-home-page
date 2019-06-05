e941abc6be21be92bb92dd2c4545ca1748d6b1d0
9542747493 <954274793@qq.com>
2019-06-05 08:36:33 +0800

    jianshu-home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,167 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="layui-row" id="banner">
+		<left style="margin-top:10%;"></left>
+		<!-- 这是右边开始 -->
+		<div id="right" class="layui-col-md4" style="margin-top:10%;padding-left:4%">
+			<ul class="layui-col-md12">
+				<li class="layui-col-md12"><a><img src="../images/right1.png"></a></li>
+				<li class="layui-col-md12"><a><img src="../images/right2.png"></a></li>
+				<li class="layui-col-md12"><a><img src="../images/right3.png"></a></li>
+				<li class="layui-col-md12"><a><img src="../images/right4.png"></a></li>
+			</ul>
+			<div class="right1 layui-col-md12" style="margin-top2%;">
+				<div class="layui-col-md4"><img class="layui-col-md4" style="width:80%; height:80%;opacity: .85;" src="../images/erweima.png"></div>
+				<div class="layui-col-md8" style="margin-top: 10px;">
+						<a href="#" class="lyui-col-md12" style="font-size:15px;color:#333;display: block;width: 140px; text-decoration: none;">下载简书手机App&nbsp;<i class="layui-icon layui-icon-right"></i></a>
+						<p class="lyui-col-md12" style="margin-top:4px;font-size:13px;color:#999;display: block;width: 150px;">随时随地发现和创作内容</p>
+				</div>
+			</div>
+			<div class="right2 layui-col-md12">
+				<img style="border-radius:10px;" class="right2 layui-col-md12" src="../images/guanggao.jpg" />
+				<button>广告</button>
+			</div>
+
+			<div class="right2 layui-col-md12" style="margin-top:5%;">
+				<div class="right2 layui-col-md12">
+					<span style="font-size:14px;color: #969696;float: left;">推荐作者</span>
+					<a style="display: inline-block;font-size: 14px;color: #969696;float: right; text-decoration: none; cursor: pointer;"><i class="layui-icon layui-icon-refresh">&nbsp;换一批</i></a>
+				</div>
+				<div class="layui-col-md12" style="margin-top:4%;">
+					<div class="layui-col-md3">
+						<img style="width:79.5%;border:1px solid #ddd;border-radius:50%;" src="../images/chadian.webp" />
+					</div>
+					<div class="layui-col-md9">
+						<div class="layui-col-md12" style="margin-top:1%;">
+							<h1 style="padding-top:5px;margin-right:60px;font-size: 14px;display: block;float: left;">茶点故事</h1>
+							<a style="float:right;margin-top:5px;padding-left:0;font-size:13px;color:#42c02e;"><i class="layui-icon layui-icon-add-1"></i>关注</a>
+							<p class="layui-col-md12" style="margin-top:3%;font-size: 12px;color: #969696;">写了311.8k字 · 10.1k喜欢</p>
+						</div>
+					</div>
+				</div>
+				<div class="layui-col-md12" style="margin-top:4%;">
+					<div class="layui-col-md3">
+						<img style="width:79.5%;border: 1px solid #ddd;border-radius: 50%;" src="../images/bing.webp" />
+					</div>
+					<div class="layui-col-md9">
+						<div class="layui-col-md12" style="margin-top:1%;">
+							<h1 style="padding-top:5px;margin-right:60px;font-size: 14px;display: block;float: left;">冰千里</h1>
+							<a style="float:right;margin-top:5px;padding-left:0;font-size:13px;color:#42c02e;"><i class="layui-icon layui-icon-add-1"></i>关注</a>
+							<p class="layui-col-md12" style="margin-top:3%;font-size: 12px;color: #969696;">写了497.9k字 · 8.7k喜欢</p>
+						</div>
+					</div>
+				</div>
+				<div class="layui-col-md12" style="margin-top:4%;">
+					<div class="layui-col-md3">
+						<img style="width:79.5%;border: 1px solid #ddd;border-radius: 50%;" src="../images/hangju.webp" />
+					</div>
+					<div class="layui-col-md9">
+						<div class="layui-col-md12" style="margin-top:1%;">
+							<h1 style="padding-top:5px;margin-right:60px;font-size: 14px;display: block;float: left;">行距版君</h1>
+							<a style="float:right;margin-top:5px;padding-left:0;font-size:13px;color:#42c02e;"><i class="layui-icon layui-icon-add-1"></i>关注</a>
+							<p class="layui-col-md12" style="margin-top:3%;font-size: 12px;color: #969696;">写了500.2k字 · 53.9k喜欢</p>
+						</div>
+					</div>
+				</div>
+				<div class="layui-col-md12" style="margin-top:4%;">
+					<div class="layui-col-md3">
+						<img style="width:79.5%;;border: 1px solid #ddd;border-radius: 50%;" src="../images/lostdays.webp" />
+					</div>
+					<div class="layui-col-md9">
+						<div class="layui-col-md12" style="margin-top:1%;">
+							<h1 style="padding-top:5px;margin-right:60px;font-size: 14px;display: block;float: left;">lostdays</h1>
+							<a style="float:right;margin-top:5px;padding-left:0;font-size:13px;color:#42c02e;"><i class="layui-icon layui-icon-add-1"></i>关注</a>
+							<p class="layui-col-md12" style="margin-top:3%;font-size: 12px;color: #969696;">写了231.7k字 · 2.1k喜欢</p>
+						</div>
+					</div>
+				</div>
+				<div class="layui-col-md12" style="margin-top:4%;">
+					<div class="layui-col-md3">
+						<img style="width:79.5%;border:1px solid #ddd;border-radius: 50%;" src="../images/wuxian.webp" />
+					</div>
+					<div class="layui-col-md9">
+						<div class="layui-col-md12" style="margin-top:1%;">
+							<h1 style="padding-top:5px;margin-right:60px;font-size: 14px;display: block;float: left;">无限猴子</h1>
+							<a style="float:right;margin-top:5px;padding-left:0;font-size:13px;color:#42c02e;"><i class="layui-icon layui-icon-add-1"></i>关注</a>
+							<p class="layui-col-md12" style="margin-top:3%;font-size: 12px;color: #969696;">写了311.8k字 · 10.1k喜欢</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="right3 layui-col-md12">
+				<a class="layui-col-md12" style="text-align: center; text-decoration: none; cursor: pointer;" href="/tjzz">查看全部
+					<i class="layui-icon layui-icon-right"></i></a>
+			</div>
+			<div style="margin-top: 5%;" class="right2 layui-col-md12">
+				<img style="border-radius:10px;" class="right2 layui-col-md12" src="../images/guanggo2.jpg" />
+				<button>广告</button>
+			</div>
+		</div>
+		<!-- 这是右边结束 -->
+	</div>
+	
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	import left from './left.vue'
+	export default {
+		name: 'app',
+		components: {
+			left: left,
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped>
+	#banner {
+		width: 65%;
+		margin: 0 auto;
+	}
+	#right{
+		
+	}
+	#left {
+		background: #0000FF;
+	}
+
+	#right img {
+		width: 100%;
+		min-height: 50px;
+		margin-bottom: 6px;
+		border-radius: 4px;
+	}
+
+	.right1 {
+		margin-bottom: 30px;
+		padding: 10px 22px;
+		width: 100%;
+		border: 1px solid #f0f0f0;
+		border-radius: 6px;
+		background-color: #fff;
+	}
+
+	.right2 button {
+		position: absolute;
+		border: none;
+		bottom: 5px;
+		right: 0;
+		color: #fff;
+		background-color: #000;
+		padding: 0 5px;
+		font-size: 12px;
+		line-height: 20px;
+		opacity: 0.7;
+	}
+
+	.right3 {
+		padding: 7px 7px 7px 12px;
+		width: 100%;
+		font-size: 13px;
+		color: #787878;
+		background-color: #f7f7f7;
+		border: 1px solid #dcdcdc;
+		border-radius: 4px;
+	}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './components/Home.vue'
+import Tjzz from './components/Tjzz.vue'
+import denglu from './components/denglu.vue'
+import zhuce from './components/zhuce.vue'
 
 Vue.use(Router)
 
@@ -13,13 +16,17 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+		{
+			path:'/tjzz',
+			component:Tjzz
+		},
+		{
+			path:'/denglu',
+			component:denglu
+		},
+		{
+			path:'/zhuce',
+			component:zhuce
+		}
   ]
 })
